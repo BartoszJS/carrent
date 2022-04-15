625a84186191ec89ad4265cb243da92811869121
BartoszJS <bartosz290300@wp.pl>
2022-04-15 12:13:10 +0200

    1 header

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+
+<header class="header">
+    
+    <div class="inner_header">
+        <label class="logo">
+            <div class="logo_container">
+                <a href="index.php"><h1>Car<span>Rent</span> </h1></a>
+            </div>
+        </label>
+        <ul class="navigatione">
+            <a class="active" href="oferty.php"><li><i class="fa fa-search"></i> <br> Przegląd ofert <div class="przerwa"><br></div></li> </a>
+            <a class="active" href="kontakt.php"><li> <i class="fa fa-plus"></i>  <br> Kontakt  <div class="przerwa"><br></div></li> </a>
+            <a class="active" href="logowanie.php"><li><i class="fa fa-search"></i> <br> Logowanie  <div class="przerwa"><br></div></li> </a>
+            
+        </ul>
+
+    </div>
+  
+</header>
